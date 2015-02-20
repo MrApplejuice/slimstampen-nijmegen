137e38f1c41dd787787d2c29d13e0c8eb1866634
Paul Konstantin Gerke <no@e.mail>
2015-02-20 20:53:09 +0100

    Fixed: image aspect ratios Fixed: specifying program argument "help" now only shows help and does not start the program Fixed: one actually needs to specify "fullscreen" to switch the application into fullscreen mode

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 
 from visual import MovieVisualizer
@@ -27,7 +29,7 @@ class LearnWordViewer(object):
   def __init__(self, win):
     self.__win = win
     
-    self.imageComponent = visual.ImageStim(win, pos=(0, 0.5), size=0.5)
+    self.imageComponent = visual.ImageStim(win, pos=(0, 0.5))
     self.wordText = visual.TextStim(win, pos=(-.25, -0.2))
     self.translationText = visual.TextStim(win, pos=( .25, -0.2))
     
@@ -37,7 +39,10 @@ class LearnWordViewer(object):
     
     TEXT_HEIGHT = 0.1
     
+    self.imageComponent.size = None
     self.imageComponent.image = image
+    self.imageComponent.size *= 0.8 / self.imageComponent.size[1]
+
     self.wordText.text = word
     self.translationText.text = translation
     
@@ -107,9 +112,11 @@ class TestWordViewer(object):
     
     self.typedText.autoDraw = True
     
-    history = recordKeyboardInputs(self.__win, self.typedText)
-    
-    typedWord = "" if len(history) == 0 else history[-1]["current_text"]
+    typedWord = None
+    while not typedWord:    
+      history = recordKeyboardInputs(self.__win, self.typedText)
+      typedWord = None if len(history) == 0 else history[-1]["current_text"].strip()
+
     response = checkResponseFunction(typedWord)
     if response != ApplicationInterface.Response.NONE:
       if response == ApplicationInterface.Response.CORRECT:
@@ -150,32 +157,33 @@ Usage:
 Parameter:
   fullscreen    Specify the parameter "fullscreen" to start the application in fullscreen mode
 """
-  fullscreenMode = "windowed" in sys.argv[1:]
-  
-  print "Starting in", "fullscreen" if fullscreenMode else "window", "mode"
-  
-  mainWindow = visual.Window(fullscr=fullscreenMode, size=(1280, 720))
+  else:
+    fullscreenMode = "fullscreen" in sys.argv[1:]
+    
+    print "Starting in", "fullscreen" if fullscreenMode else "window", "mode"
+    
+    mainWindow = visual.Window(fullscr=fullscreenMode, size=(1280, 720))
 
-  # Load stimuli
-  stimuli = CSVDictList()
-  stimuli.load("testdata/test.csv")
-  
-  # Initialize different "scenes"
-  movieViewer = MovieViewer(mainWindow)
-  learnWordViewer = LearnWordViewer(mainWindow)
-  testWordViewer = TestWordViewer(mainWindow)
-  highscoreHighscoreViewer = HighscoreViewer(mainWindow)
-  
-  #movieViewer.playMovie("/media/crepo/TEMP/Mnemonic_task/stimuli/MemrisePrizev3.wmv")
-  
-  class ThisAppInterface(ApplicationInterface):
-    def learn(self, image, word, translation):
-      learnWordViewer.show(image, word, translation)
-    def test(self, word, checkResponseFunction):
-      return testWordViewer.test(word, checkResponseFunction)
-    def updateHighscore(self, score):
-      highscoreHighscoreViewer.updateHighscore(score)
-      
-  assignmentModel = AssignmentModel(ThisAppInterface(), stimuli)
+    # Load stimuli
+    stimuli = CSVDictList()
+    stimuli.load("testdata/test.csv")
+    
+    # Initialize different "scenes"
+    movieViewer = MovieViewer(mainWindow)
+    learnWordViewer = LearnWordViewer(mainWindow)
+    testWordViewer = TestWordViewer(mainWindow)
+    highscoreHighscoreViewer = HighscoreViewer(mainWindow)
+    
+    #movieViewer.playMovie("/media/crepo/TEMP/Mnemonic_task/stimuli/MemrisePrizev3.wmv")
+    
+    class ThisAppInterface(ApplicationInterface):
+      def learn(self, image, word, translation):
+        learnWordViewer.show(image, word, translation)
+      def test(self, word, checkResponseFunction):
+        return testWordViewer.test(word, checkResponseFunction)
+      def updateHighscore(self, score):
+        highscoreHighscoreViewer.updateHighscore(score)
+        
+    assignmentModel = AssignmentModel(ThisAppInterface(), stimuli)
 
-  assignmentModel.run()
+    assignmentModel.run()
